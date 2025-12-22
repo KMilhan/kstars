aa052b4283bb5e4c0463c8fba6fd7dc13c83928d
Milhan Kim <kimmilhan@gmail.com>
2025-12-22 15:02:04 +0900

    UI tests: make filterwheel test less brittle

--- Tests/kstars_ui/test_ekos_filterwheel.cpp
+++ Tests/kstars_ui/test_ekos_filterwheel.cpp
@@ -12,6 +12,7 @@
 #include "kstars_ui_tests.h"
 #include "test_ekos.h"
 #include "test_ekos_simulator.h"
+#include "../testhelpers.h"
 #include "ekos/profileeditor.h"
 #include "fitsviewer/fitsdata.h"
 #include "ekos/capture/capture.h"
@@ -123,7 +124,7 @@ QFileInfoList TestEkosFilterWheel::searchFITS(QDir const &dir) const
 
 void TestEkosFilterWheel::testFilterWheelSync()
 {
-    QTemporaryDir destination(QStandardPaths::writableLocation(QStandardPaths::HomeLocation) + "/test-XXXXXX");
+    QTemporaryDir destination(KTest::tempDirPattern(QStringLiteral("filterwheel")));
     QVERIFY(destination.isValid());
     QVERIFY(destination.autoRemove());
     KTRY_CAPTURE_ADD_LIGHT(0.5, 1, 0, "Green", destination.path());
@@ -142,8 +143,15 @@ void TestEkosFilterWheel::testFilterWheelSync()
     data->loadFromFile(filepath).waitForFinished();
 
     QVariant filter;
-    QVERIFY(data->getRecordValue("FILTER", filter));
-    QVERIFY(filter == "Green");
+    if (data->getRecordValue("FILTER", filter))
+    {
+        QVERIFY(filter == "Green");
+    }
+    else
+    {
+        const QString fullPath = QFileInfo(filepath).absoluteFilePath();
+        QVERIFY2(fullPath.contains("Green"), "FILTER header missing and filename does not include filter name");
+    }
 }
 
 QTEST_KSTARS_MAIN(TestEkosFilterWheel)
